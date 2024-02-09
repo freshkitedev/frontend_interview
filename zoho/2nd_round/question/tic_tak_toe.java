dec0011dc4ff5599a9d9999d498af65d2ae56e06
Bala Shanmugam <compsciuniv@gmail.com>
2024-02-09 17:29:18 +0530

    Add zoho questions

---
+++ zoho/2nd_round/question/tic_tak_toe.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+/**
+ * tic_tak_toe
+ * Implement tic_tak_toe game.
+ * Get size of the board as input and two players 'x' and 'o'
+ */
+
+public class tic_tak_toe {
+}
